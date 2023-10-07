050f0cf8e22f4b13414c45ebad974d0a9491bff9
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2023-10-08 00:19:52 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,31 @@
+import createCampaign from './create-campaign.jpeg';
+import dashboard from './dashboard.png';
+import logo from './logo.jpg';
+import logout from './logout.png';
+import payment from './payment.png';
+import profile from './download.png';
+import sun from './sun.png';
+import withdraw from './withdraw.png';
+import tagType from './type.png';
+import search from './search.png';
+import menu from './menu.png';
+import money from './money.png';
+import loader from './loader.png';
+import thirdweb from './thirdweb.png';
+
+export {
+  tagType,
+  createCampaign,
+  dashboard,
+  logo,
+  logout,
+  payment,
+  profile,
+  sun,
+  withdraw,
+  search,
+  menu,
+  money,
+  loader,
+  thirdweb,
+};
